63a30327186c87867fc42a92b675ef24d14ffe63
Concourse <concourse@finbourne.com>
2024-03-01 18:01:19 +0000

    Updates for v2.0.27

--- sdk/lusid_workflow/api/workers_api.py
+++ sdk/lusid_workflow/api/workers_api.py
@@ -839,7 +839,7 @@ class WorkersApi:
         _auth_settings = ['oauth2']  # noqa: E501
 
         _response_types_map = {
-            '201': "PagedResourceListOfWorker",
+            '200': "PagedResourceListOfWorker",
             '400': "LusidValidationProblemDetails",
         }
 
